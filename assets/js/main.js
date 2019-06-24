29d9c3e8202fa181129448f712d1f7a79cb61d65
junior <zukisanante@gmail.com>
2019-06-24 16:32:31 +0200

    variables declared and an array to hold cards

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+//Declaring variables
+var cards_memory = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L'];
+var values_memory = [];
+var tile_id_memory = [];
+var flip_tiles = 0;
+//Defining an array to flip th cards
+Array.memory.tile_memory_shuffle = function() {
+    var i = this.length,
+        j, temp;
+    while (--i > 0) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = this[j];
+        this[j] = this[i];
+        this[i] = temp;
+    }
+}
