96906e5e4f8f6ae2565cafd3713a3bb9280bca02
junior <zukisanante@gmail.com>
2019-06-24 17:01:46 +0200

    assets/css/main.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,19 @@ var values_memory = [];
 var tile_id_memory = [];
 var flip_tiles = 0;
 //Defining an array to flip th cards
-Array.memory.tile_memory_shuffle = function() {
-    var i = this.length,
-        j, temp;
-    while (--i > 0) {
-        j = Math.floor(Math.random() * (i + 1));
-        temp = this[j];
-        this[j] = this[i];
-        this[i] = temp;
+Array.prototype.tile_memory_shuffle = function() {
+        var i = this.length,
+            j, temp;
+        while (--i > 0) {
+            j = Math.floor(Math.random() * (i + 1));
+            temp = this[j];
+            this[j] = this[i];
+            this[i] = temp;
+        }
     }
+    // Generate a new card by flipping the open back
+function new_Card() {
+    flip_tiles = 0;
+    var output = "";
+    cards_memory.tile_memory_shuffle();
 }
